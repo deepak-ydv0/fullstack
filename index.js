07412a7520605d8d50f1455c0be98d4f8ec8b18e
DPK-YDV <kd806040@gmail.com>
2025-03-24 22:13:57 +0530

    Add user routes and connect to the database in the main application file

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import db from "./utills/db.js";
 
+//import routes
+import userRoutes from "./routes/user.routes.js";
+
 dotenv.config();
 
 const app = express();
@@ -23,7 +26,12 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
-db()
+
+//connect Db
+db();
+
+//user routes
+app.use("/api/v1/users", userRoutes);
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
